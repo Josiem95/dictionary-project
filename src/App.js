03d20033a2c4ba2e36ae36f558537e48441313b8
Josie Mortison <81780952+Josiem95@users.noreply.github.com>
2021-10-18 17:05:38 -0700

    search form added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
-
+import Dictionary from "./Dictionary";
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">Dictionary</header>
+    <div className="container">
+      <div className="App">
+        <header className="App-header">Dictionary</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Josie Mortison</footer>
+      </div>
     </div>
   );
 }
